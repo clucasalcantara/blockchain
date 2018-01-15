ab332ec0cf9b0eb13336fcaf8480ce2af48c9f64
Caio Alcantara <alcantaracaiolucas@gmail.com>
2018-01-15 11:08:37 +0000

    Add a  initial and shallow blockchain structure

---
+++ index.js
@@ -0,0 +1,5 @@
+const Chain = require('./modules/Chain')
+
+const BlockChain = new Chain()
+
+console.log(BlockChain.getLastBlock())
---
+++ tools/blockchain-integrity.js
@@ -0,0 +1,21 @@
+const isValid = () => {
+    for (let i = 1; i < this.blocks.length; i++) {
+        const currentBlock = this.blocks[i]
+        const previousBlock = this.blocks[i - 1]
+
+        if (currentBlock.hash !== currentBlock.generateHash()) {
+            return false
+        }
+
+        if (currentBlock.index !== previousBlock.index + 1) {
+            return false
+        }
+
+        if (currentBlock.previousHash !== previousBlock.hash) {
+            return false
+        }
+    }
+    return true
+}
+
+export default isValid
